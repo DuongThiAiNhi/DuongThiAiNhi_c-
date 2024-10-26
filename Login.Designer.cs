6f9c33cea0c7e65189973204e9b150e73686a618
Ai Nhi <caocao2082004hi@gmail.com>
2024-10-26 10:08:07 +0700

    kiemtra1

---
+++ Login.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace QuanLyQuanCafe
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.tbPassWord = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.gbInformation = new System.Windows.Forms.GroupBox();
+            this.gbInformation.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(138, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOG IN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Password";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(117, 32);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(210, 22);
+            this.tbUserName.TabIndex = 1;
+            // 
+            // tbPassWord
+            // 
+            this.tbPassWord.Location = new System.Drawing.Point(117, 70);
+            this.tbPassWord.Name = "tbPassWord";
+            this.tbPassWord.Size = new System.Drawing.Size(210, 22);
+            this.tbPassWord.TabIndex = 3;
+            this.tbPassWord.UseSystemPasswordChar = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(171, 123);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Log In";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(252, 123);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(117, 98);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(111, 19);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Show Password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // gbInformation
+            // 
+            this.gbInformation.Controls.Add(this.checkBox1);
+            this.gbInformation.Controls.Add(this.btnExit);
+            this.gbInformation.Controls.Add(this.btnLogin);
+            this.gbInformation.Controls.Add(this.tbPassWord);
+            this.gbInformation.Controls.Add(this.tbUserName);
+            this.gbInformation.Controls.Add(this.label3);
+            this.gbInformation.Controls.Add(this.label2);
+            this.gbInformation.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbInformation.Location = new System.Drawing.Point(12, 64);
+            this.gbInformation.Name = "gbInformation";
+            this.gbInformation.Size = new System.Drawing.Size(338, 165);
+            this.gbInformation.TabIndex = 1;
+            this.gbInformation.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AcceptButton = this.btnLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(365, 247);
+            this.Controls.Add(this.gbInformation);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.Text = "Đăng nhập";
+            this.gbInformation.ResumeLayout(false);
+            this.gbInformation.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.TextBox tbPassWord;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox gbInformation;
+    }
+}
+
---
+++ UserManager.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace QuanLyQuanCafe
+{
+    public partial class UserManager : Form
+    {
+        public List<Manager> lstMana = new List<Manager>();
+        private BindingSource bs = new BindingSource();
+        public bool isThoat = true;
+        public event EventHandler LogOut;
+
+        private string managerImagePath = string.Empty;
+        public UserManager()
+        {
+            InitializeComponent();
+            //ngay sinh
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "dd MMMM yyyy";
+            // Handle value changes (optional)
+            dateTimePicker1.ShowUpDown = true;
+        }
+
+        public List<Manager> GetData()
+        {
+            // Sample data can be added here if needed
+            return lstMana;
+        }
+        private void SetupDataGridView()
+        {
+            dgvManager.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvManager.Columns[0].HeaderText = "ID";
+            dgvManager.Columns[1].HeaderText = "Name";
+            dgvManager.Columns[2].HeaderText = "Phone Number";
+            dgvManager.Columns[3].HeaderText = "Address";
+            dgvManager.Columns[4].HeaderText = "Date of Birth";
+            dgvManager.Columns[5].HeaderText = "Gender";
+            dgvManager.Columns[6].HeaderText = "Photo";
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tbID.Enabled = true;
+
+                // Check for required fields
+                if (string.IsNullOrWhiteSpace(tbID.Text))
+                {
+                    MessageBox.Show("Lỗi: Vui lòng nhập ID hợp lệ.");
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(tbName.Text))
+                {
+                    MessageBox.Show("Lỗi: Vui lòng nhập tên hợp lệ.");
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(tbNumber.Text))
+                {
+                    MessageBox.Show("Lỗi: Vui lòng nhập số điện thoại hợp lệ.");
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(tbAddress.Text))
+                {
+                    MessageBox.Show("Lỗi: Vui lòng nhập địa chỉ hợp lệ.");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(managerImagePath))
+                {
+                    MessageBox.Show("Lỗi: Vui lòng chọn một ảnh.");
+                    return;
+                }
+
+                if (dateTimePicker1.Value == null)
+                {
+                    MessageBox.Show("Lỗi: Vui lòng chọn ngày sinh hợp lệ.");
+                    return;
+                }
+
+                int newId = int.Parse(tbID.Text);
+                if (lstMana.Any(emp => emp.Id == newId))
+                {
+                    MessageBox.Show("Lỗi: ID đã tồn tại. Vui lòng nhập ID khác.");
+                    return;
+                }
+
+                Manager newEmp = new Manager
+                {
+                    Id = newId,
+                    Name = tbName.Text,
+                    Gender = ckGender.Checked,
+                    Address = tbAddress.Text,
+                    PhoneNumber = tbNumber.Text,
+                    ImagePath = managerImagePath,
+                    BirthDate = dateTimePicker1.Value.Date
+                };
+
+                lstMana.Add(newEmp);
+                bs.ResetBindings(false);
+                ClearInputFields();
+
+                tbID.Enabled = true;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Lỗi: Vui lòng nhập số nguyên hợp lệ cho ID.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message);
+            }
+        }
+
+        private void UserManager_Load(object sender, EventArgs e)
+        {
+            lstMana = GetData();
+            bs.DataSource = lstMana;
+            dgvManager.DataSource = bs;
+            SetupDataGridView(); 
+            dateTimePicker1.Value = DateTime.Now; 
+        }
+        private void ClearInputFields()
+        {
+            tbID.Text = "";
+            tbName.Text = "";
+            tbNumber.Text = "";
+            tbAddress.Text = "";
+            ckGender.Checked = false;
+            pbManagerImage.Image = null;
+            dateTimePicker1.Value = DateTime.Now; // Reset DateTimePicker to current date
+        }
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime selectedDate = dateTimePicker1.Value;
+            // Do something with the selected date
+            this.Text = dateTimePicker1.Value.ToString("dd MMMM yyyy");
+        }
+        private void SetDateForDateTimePicker(DateTime date)
+        {
+            dateTimePicker1.Value = date; // Set a specific date, e.g. new DateTime(2024, 10, 17)
+        }
+
+        private void dgvManager_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (e.RowIndex < 0 || e.RowIndex >= lstMana.Count) return;
+
+            Manager em = lstMana[e.RowIndex];
+
+            tbID.Text = em.Id.ToString();
+            tbName.Text = em.Name;
+            tbAddress.Text = em.Address;
+            tbNumber.Text = em.PhoneNumber;
+            ckGender.Checked = em.Gender;
+
+            tbID.Enabled = false; // Disable ID field for editing
+
+            if (!string.IsNullOrEmpty(em.ImagePath) && System.IO.File.Exists(em.ImagePath))
+            {
+                pbManagerImage.Image = Image.FromFile(em.ImagePath);
+            }
+            else
+            {
+                pbManagerImage.Image = null;
+            }
+        }
+
+
+
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (dgvManager.CurrentRow == null) return;
+
+            int idx = dgvManager.CurrentRow.Index;
+            lstMana.RemoveAt(idx);
+            bs.ResetBindings(false);
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (dgvManager.CurrentRow == null) return;
+
+            int idx = dgvManager.CurrentRow.Index;
+            Manager em = lstMana[idx];
+
+            try
+            {
+                // Check for required fields
+                if (string.IsNullOrWhiteSpace(tbID.Text))
+                {
+                    MessageBox.Show("Lỗi: Vui lòng nhập ID hợp lệ.");
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(tbName.Text))
+                {
+                    MessageBox.Show("Lỗi: Vui lòng nhập tên hợp lệ.");
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(tbNumber.Text))
+                {
+                    MessageBox.Show("Lỗi: Vui lòng nhập số điện thoại hợp lệ.");
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(tbAddress.Text))
+                {
+                    MessageBox.Show("Lỗi: Vui lòng nhập địa chỉ hợp lệ.");
+                    return;
+                }
+
+                int newId = int.Parse(tbID.Text);
+                if (lstMana.Any(emp => emp.Id == newId && emp != em))
+                {
+                    MessageBox.Show("Lỗi: ID đã tồn tại. Vui lòng nhập ID khác.");
+                    return;
+                }
+
+                em.Id = newId;
+                em.Name = tbName.Text;
+                em.PhoneNumber = tbNumber.Text;
+                em.Address = tbAddress.Text;
+                em.Gender = ckGender.Checked;
+                em.ImagePath = managerImagePath;
+                em.BirthDate = dateTimePicker1.Value.Date;
+
+                bs.ResetBindings(false);
+                ClearInputFields();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Lỗi: Vui lòng nhập số nguyên hợp lệ cho ID.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message);
+            }
+        }
+
+        //private void btnThoat_Click(object sender, EventArgs e)
+        //{
+        //    Thoat?.Invoke(this, EventArgs.Empty);
+        //}
+
+        //private void btnThoat_Click_1(object sender, EventArgs e)
+        //{
+        //    Thoat?.Invoke(this, EventArgs.Empty);
+        //}
+
+        private void tbNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                MessageBox.Show("Chỉ được nhập kí tự số");
+                e.Handled = true;
+            }
+        }
+
+        private void btnSelectImage_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Image Files (*.jpg;*.jpeg;*.png)|*.jpg;*.jpeg;*.png";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    managerImagePath = ofd.FileName; // Store the image path
+                    pbManagerImage.Image = Image.FromFile(managerImagePath); // Show the image
+                }
+            }
+        }
+        private void SetupImageList()
+        {
+            ImageList imageList = new ImageList();
+            imageList.ImageSize = new Size(24, 24);
+
+            // Add images to ImageList (make sure paths are correct)
+            imageList.Images.Add(Image.FromFile("Images/add.png"));    // Index 0
+            imageList.Images.Add(Image.FromFile("Images/edit.png"));   // Index 1
+            imageList.Images.Add(Image.FromFile("Images/delete.png")); // Index 2
+
+            // Assign ImageList to buttons
+            btnThem.ImageList = imageList;
+            btnThem.ImageIndex = 0;
+
+            btnSua.ImageList = imageList;
+            btnSua.ImageIndex = 1;
+
+            btnXoa.ImageList = imageList;
+            btnXoa.ImageIndex = 2;
+        }
+
+        private void tbID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btLogOut_Click(object sender, EventArgs e)
+        {
+            LogOut?.Invoke(this, EventArgs.Empty);
+
+        }
+
+        private void btThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
