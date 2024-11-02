784cb5f94959ab12106638168d354830159e9fe5
DuongThiAiNhi <caocao2082004hi@gmail.com>
2024-11-02 13:14:30 +0700

    bai bao cao

---
+++ Login.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyQuanCafe
+{
+    public partial class Login : Form
+    {
+        string tentaikhoan = "nhi";
+        string matkhau = "1";
+        string strCon = @"Data Source=LAPTRONGVIEN\SQLEXPRESS;Initial Catalog=UserManager;User Id=sa;Trust Server Certificate=True";
+        SqlConnection sqlCon = null;
+        public Login()
+        {
+            InitializeComponent();
+        }
+        public bool kt;
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult q = MessageBox.Show("Are you exit?", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (q.Equals(DialogResult.Yes))
+            {
+                this.Close();
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (KiemTraDangNhap(tbUserName.Text, tbPassWord.Text))
+            {
+                UserManager f = new UserManager();
+                f.Show();
+                this.Hide();
+                f.LogOut += F_LogOut;
+                tbUserName.Text = "";
+                tbPassWord.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Sai tên tài khoản hoặc mật khẩu", "Error");
+                tbUserName.Focus();
+            }
+
+        }
+
+        private void F_LogOut(object sender, EventArgs e)
+        {
+            (sender as UserManager).isThoat = false;
+            (sender as UserManager).Close();
+            this.Show();
+
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                tbPassWord.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                tbPassWord.UseSystemPasswordChar = true;
+            }
+        }
+        bool KiemTraDangNhap(string tentaikhoan, string matkhau)
+        {
+            if (tentaikhoan == this.tentaikhoan && matkhau == this.matkhau)
+            {
+                return true;
+            }
+            return false;
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gbInformation_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ UserManager.Designer.cs
@@ -0,0 +1,390 @@
+﻿namespace QuanLyQuanCafe
+{
+    partial class UserManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserManager));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgvManager = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSelectImage = new System.Windows.Forms.Button();
+            this.pbManagerImage = new System.Windows.Forms.PictureBox();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.tbNumber = new System.Windows.Forms.TextBox();
+            this.tbdt = new System.Windows.Forms.Label();
+            this.ckGender = new System.Windows.Forms.CheckBox();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btLogOut = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvManager)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbManagerImage)).BeginInit();
+            this.panel7.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(364, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "USER MANAGER";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dgvManager);
+            this.panel1.Location = new System.Drawing.Point(12, 107);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(514, 376);
+            this.panel1.TabIndex = 1;
+            // 
+            // dgvManager
+            // 
+            this.dgvManager.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvManager.Location = new System.Drawing.Point(0, 5);
+            this.dgvManager.Name = "dgvManager";
+            this.dgvManager.Size = new System.Drawing.Size(508, 371);
+            this.dgvManager.TabIndex = 0;
+            this.dgvManager.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvManager_CellContentClick);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnSelectImage);
+            this.panel2.Controls.Add(this.pbManagerImage);
+            this.panel2.Controls.Add(this.panel7);
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.ckGender);
+            this.panel2.Controls.Add(this.btnSua);
+            this.panel2.Controls.Add(this.btnXoa);
+            this.panel2.Controls.Add(this.btnThem);
+            this.panel2.Controls.Add(this.panel6);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Location = new System.Drawing.Point(532, 59);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(381, 424);
+            this.panel2.TabIndex = 2;
+            // 
+            // btnSelectImage
+            // 
+            this.btnSelectImage.Location = new System.Drawing.Point(256, 213);
+            this.btnSelectImage.Name = "btnSelectImage";
+            this.btnSelectImage.Size = new System.Drawing.Size(90, 29);
+            this.btnSelectImage.TabIndex = 9;
+            this.btnSelectImage.Text = "Choose file...";
+            this.btnSelectImage.UseVisualStyleBackColor = true;
+            this.btnSelectImage.Click += new System.EventHandler(this.btnSelectImage_Click);
+            // 
+            // pbManagerImage
+            // 
+            this.pbManagerImage.Location = new System.Drawing.Point(137, 213);
+            this.pbManagerImage.Name = "pbManagerImage";
+            this.pbManagerImage.Size = new System.Drawing.Size(113, 107);
+            this.pbManagerImage.TabIndex = 8;
+            this.pbManagerImage.TabStop = false;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.tbAddress);
+            this.panel7.Controls.Add(this.label6);
+            this.panel7.Location = new System.Drawing.Point(12, 133);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(334, 34);
+            this.panel7.TabIndex = 4;
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(100, 8);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(178, 20);
+            this.tbAddress.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 11);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Address";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.tbNumber);
+            this.panel5.Controls.Add(this.tbdt);
+            this.panel5.Location = new System.Drawing.Point(12, 93);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(334, 34);
+            this.panel5.TabIndex = 3;
+            // 
+            // tbNumber
+            // 
+            this.tbNumber.Location = new System.Drawing.Point(100, 8);
+            this.tbNumber.Name = "tbNumber";
+            this.tbNumber.Size = new System.Drawing.Size(178, 20);
+            this.tbNumber.TabIndex = 1;
+            this.tbNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbNumber_KeyPress);
+            // 
+            // tbdt
+            // 
+            this.tbdt.AutoSize = true;
+            this.tbdt.Location = new System.Drawing.Point(13, 11);
+            this.tbdt.Name = "tbdt";
+            this.tbdt.Size = new System.Drawing.Size(75, 13);
+            this.tbdt.TabIndex = 0;
+            this.tbdt.Text = "PhoneNumber";
+            // 
+            // ckGender
+            // 
+            this.ckGender.AutoSize = true;
+            this.ckGender.Location = new System.Drawing.Point(51, 213);
+            this.ckGender.Name = "ckGender";
+            this.ckGender.Size = new System.Drawing.Size(60, 17);
+            this.ckGender.TabIndex = 7;
+            this.ckGender.Text = "Female";
+            this.ckGender.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(150, 380);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(72, 41);
+            this.btnSua.TabIndex = 5;
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(281, 380);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(76, 41);
+            this.btnXoa.TabIndex = 4;
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(12, 380);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 41);
+            this.btnThem.TabIndex = 3;
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.dateTimePicker1);
+            this.panel6.Controls.Add(this.label5);
+            this.panel6.Location = new System.Drawing.Point(12, 173);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(334, 34);
+            this.panel6.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(100, 5);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(178, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "DateOfBirth";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.tbName);
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Location = new System.Drawing.Point(12, 53);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(334, 34);
+            this.panel4.TabIndex = 2;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(100, 8);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(178, 20);
+            this.tbName.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Name";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.tbID);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(12, 13);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(334, 34);
+            this.panel3.TabIndex = 0;
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(100, 8);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(178, 20);
+            this.tbID.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "ID";
+            // 
+            // btLogOut
+            // 
+            this.btLogOut.Location = new System.Drawing.Point(860, 17);
+            this.btLogOut.Name = "btLogOut";
+            this.btLogOut.Size = new System.Drawing.Size(53, 27);
+            this.btLogOut.TabIndex = 3;
+            this.btLogOut.UseVisualStyleBackColor = true;
+            this.btLogOut.Click += new System.EventHandler(this.btLogOut_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(271, 75);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Search...";
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Location = new System.Drawing.Point(12, 72);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(253, 20);
+            this.txtTimKiem.TabIndex = 7;
+            this.txtTimKiem.TextChanged += new System.EventHandler(this.txtTimKiem_TextChanged);
+            // 
+            // UserManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(925, 518);
+            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btLogOut);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "UserManager";
+            this.Text = "UserManager";
+            this.Load += new System.EventHandler(this.UserManager_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvManager)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbManagerImage)).EndInit();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.CheckBox ckGender;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dgvManager;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label tbdt;
+        private System.Windows.Forms.TextBox tbNumber;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnSelectImage;
+        private System.Windows.Forms.PictureBox pbManagerImage;
+        private System.Windows.Forms.Button btLogOut;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtTimKiem;
+    }
+}
